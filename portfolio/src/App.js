4d6c3ef12075da8707a4eabf6a421af4eb9d8a46
Zach Zukowski <zzukowski@ejfcap.com>
2020-07-15 21:07:41 -0400

    cleaner

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -58,16 +58,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Zach Z
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+     
+        
+        
         <MyGallery/>
       </header>
     </div>
